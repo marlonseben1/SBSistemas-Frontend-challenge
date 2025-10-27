27da3e9070a726698fe62de2eb13a17492000e43
marlonseben <marlonseben1@gmail.com>
2025-10-27 12:54:50 -0300

    feat(api): criado método create em posts.ts

--- src/api/posts/posts.ts
+++ src/api/posts/posts.ts
@@ -1,11 +1,18 @@
 import { api } from "../api";
 import type { Post } from "./posts.types";
+import axios from "axios";
 
 const get = async (userId: number): Promise<Post[]> => {
   const { data } = await api.get<Post[]>(`users/${userId}/posts`);
   return data;
 };
 
+const create = async (title: string, body: string, userId: number) => {
+  const res = await axios.post(`https://jsonplaceholder.typicode.com/posts`, {title, body, userId})
+  return res;
+}
+
 export const postsApi = {
   get,
+  create
 };
